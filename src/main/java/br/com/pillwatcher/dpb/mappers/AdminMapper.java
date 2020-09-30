abd018fe5daa9a4ad9810a326ea556bafa530baa
Filipe Dias <lipao.dias@hotmail.com>
2020-09-30 08:49:33 -0300

    :white_check_mark: Adding AdminService tests

--- src/main/java/br/com/pillwatcher/dpb/mappers/AdminMapper.java
+++ src/main/java/br/com/pillwatcher/dpb/mappers/AdminMapper.java
@@ -4,11 +4,15 @@ import br.com.pillwatcher.dpb.entities.Admin;
 import io.swagger.model.AdminDTOForCreate;
 import io.swagger.model.AdminDTOForResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mappings;
 import org.mapstruct.NullValueCheckStrategy;
 
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface AdminMapper {
 
+    @Mappings({
+            
+    })
     Admin toAdminForCreateEntity(AdminDTOForCreate adminDTOForCreate);
 
     AdminDTOForCreate toAdminForCreate(Admin admin);
---
+++ src/test/java/br/com/pillwatcher/dpb/services/AdminServiceTest.java
@@ -0,0 +1,67 @@
+package br.com.pillwatcher.dpb.services;
+
+import br.com.pillwatcher.dpb.entities.Admin;
+import br.com.pillwatcher.dpb.exceptions.AdminException;
+import br.com.pillwatcher.dpb.mappers.AdminMapper;
+import br.com.pillwatcher.dpb.repositories.AdminRepository;
+import io.swagger.model.AdminDTOForCreate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static br.com.pillwatcher.dpb.constants.TestConstants.getAdmin;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static br.com.pillwatcher.dpb.constants.TestConstants.getAdminDtoForCreate;
+
+@SpringBootTest
+class AdminServiceTest {
+
+    @Autowired
+    private AdminService service;
+
+    @Autowired
+    private AdminMapper mapper;
+
+    @MockBean
+    private AdminRepository repository;
+
+    @Test
+    void createAdminShouldReturnCreatedStatus() {
+        //given
+        AdminDTOForCreate admin = getAdminDtoForCreate();
+
+        //when
+        when(repository.findAdminByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(repository.save(any(Admin.class)))
+                .thenReturn(mapper.toAdminForCreateEntity(admin));
+
+        //then
+        Admin saved = service.create(admin);
+        assertNotNull(saved);
+    }
+
+    @Test
+    void creatAdminShouldReturnAlreadyExistsStatus() {
+        Assertions.assertThrows(AdminException.class, () -> {
+            //given
+            AdminDTOForCreate admin = getAdminDtoForCreate();
+
+            //when
+            when(repository.findAdminByEmail(anyString()))
+                    .thenReturn(Optional.of(getAdmin()));
+
+            //then
+            Admin saved = service.create(admin);
+        });
+    }
+
+}
